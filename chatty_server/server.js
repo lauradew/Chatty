dea136cc371903c3f4d29e15043393dc5a9615a4
Laura De Witt <lauramdewitt@gmail.com>
2018-02-21 01:36:59 +0000

    server connects with app to display new messages

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const SocketServer = WebSocket.Server;
+const uuidv1 = require('uuid/v1');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -16,8 +18,30 @@ const wss = new SocketServer({ server });
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
+
+  ws.on('open', function open() {
+  // ws.send('something');
+    console.log('connection opened');
+  });
+
+  ws.on('message', function incoming(data) {
+    const newMessage = JSON.parse(data);
+    newMessage.id = uuidv1();
+    const messageString = JSON.stringify(newMessage);
+    // console.log(newMessage);
+    console.log('new message received:', messageString);
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        console.log('sending', messageString);
+        client.send(messageString)
+      }
+    });
+  });
+
+
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
--- src/App.jsx
+++ src/App.jsx
@@ -37,28 +37,28 @@ class App extends Component {
 // content: ''
   newMessage(userName, messageText) {
     const newMessageObj = {
-      id: Math.random(),
       type: 'user',
       username: userName,
       content: messageText
     };
-    const newMessages = this.state.messages.concat(newMessageObj);
-    this.setState({
-      messages: newMessages
-    });
+    console.log('about to send:', newMessageObj);
+    this.socket.send(JSON.stringify(newMessageObj));
   }
 
   componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket = new WebSocket("ws://localhost:3001");
+    this.socket.onopen = (openEvent) => {
+      console.log("connected to WebSocket")
+    };
+
+    this.socket.onmessage = (evt) => {
+      const newMessageObj = JSON.parse(evt.data);
+      console.log('received message from web socket:', newMessageObj);
+      const newMessages = this.state.messages.concat(newMessageObj);
+      this.setState({
+        messages: newMessages
+      });
+    };
   }
 
   render() {
